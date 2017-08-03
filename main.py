71775bd4e1ceb6e25fcb32f01428b5c259c8230d
Ankit Aggarwal <ankagar3@gmail.com>
2017-08-03 22:20:24 +0530

    spyname and salutation

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
 print "Let\'s get started"
+spy_name = raw_input("What's your name")
+if len(spy_name) > 0:
+    print 'Welcome '+ spy_name + ' We are glad to have you back'
+    spy_salutation = raw_input("What should we call you (Mr. or Ms.)?")
+    spy_name = spy_salutation + " " + spy_name
+    print spy_name
 
