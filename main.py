7b54d8d5a0a80d77e2e44e9d2cfc44698558a778
Ankit Aggarwal <ankagar3@gmail.com>
2017-08-19 13:21:12 +0530

    chat

--- main.py
+++ main.py
@@ -1,54 +1,65 @@
-from spy_details import spy_name, spy_salutation, spy_rating, spy_age, spy_is_online
+from spy_details import spy
 print "Let\'s get started"
 
-existing = raw_input("Do you want to continue as " + spy_salutation + " " + spy_name + " (Y/N)? ")
-
-def start_chat(spy_name,spy_age, spy_rating):
-
-    print "Authentication complete. Welcome " + spy_name + " age: " + str(spy_age) + " and rating of: " + str(
-        spy_rating) + " Proud to have you onboard"
+existing = raw_input("Do you want to continue as " + spy['salutation'] + " " + spy['name'] + " (Y/N): ")
 
 
-if existing == "Y":
-    start_chat(spy_name,spy_age, spy_rating)
-else:
-    spy_name = ''
-    spy_salutation = ''
-    spy_age = 0
-    spy_rating = 0.0
-    spy_is_online = False
+if existing.upper() == "Y":
+    # user wants to continue as default user.
+
+    # concatination of salutation and name of spy.
+    spy['name'] = spy['salutation'] + " " + spy['name']
+
+    # starting chat application.
+    start_chat(spy['name'], spy['age'], spy['rating'], spy['is_online'])
 
 
-    spy_name = raw_input("Welcome to spy chat, you must tell me your spy name first: ")
+elif existing.upper() == "N":
+    # user wants to continue as new user
+    spy['name'] = raw_input("Welcome to spy chat, you must tell me your spy name first:")
+    # check whether spy has input something or not
 
-    if len(spy_name) > 0:
+    if len(spy['name']) > 0:
+        print 'Welcome ' + spy['name'] + '. Glad to have you back.'
+        spy['salutation'] = raw_input("What should we all you Mister or Miss? : ")
+        # concatination of salutation and name of spy.
+        spy['name'] = spy['salutation'] + " " + spy['name']
 
-        print 'Welcome ' + spy_name + '. Glad to have you back.'
-        spy_salutation = raw_input("Should I call you Mister or Miss?: ")
-        spy_name = spy_salutation + " " + spy_name
+        print "Alright " + spy['name'] + ". I'd like to know a little bit more about you before we proceed..."
 
-        print "Alright " + spy_name + ". I'd like to know a little bit more about you before we proceed..."
-
-        spy_age = int(raw_input("What is your age?"))
-        if 12 < spy_age < 50:
-            spy_rating = float(raw_input("What is your spy rating?"))
-
-            if spy_rating > 4.5:
-                print ' You are one of the Best Rated Spy'
-            elif 3.5 < spy_rating <= 4.5:
-             print 'Good rated Spy.'
-            elif  2.5 <= spy_rating <= 3.5:
-                print 'Hard Working Spy '
-            else:
-                print 'Can perform better.'
-
-            spy_is_online = True
-
-            start_chat(spy_name, spy_age, spy_rating)
-
-
+        spy['age'] = int(raw_input("Enter your age. ?"))  # converting users input to integer (typecasting)
+        spy['rating'] = float(raw_input("What is your spy rating?"))  # converting users input to float (typecasting)
+        spy['is_online'] = True
+        if spy['rating'] > 4.5:
+            print ' You are one of the Best Rated Spy'
+        elif 3.5 < spy['rating'] <= 4.5:
+            print 'You are a good rated Spy.'
+        elif 2.5 <= spy['rating'] <= 3.5:
+            print 'You are a hard Working Spy '
         else:
-            print 'Sorry you are not eligible to be a spy'
+            print 'You Can perform better as a spy.'
 
+        # starting chat application.
+        start_chat(spy['name'], spy['age'], spy['rating'], spy['is_online'])
     else:
-        print "A spy needs to have a valid name. Try again please."
+        print "Invalid name. Try again."
+else:
+    print "Wrong choice. Try again."
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- spy_details.py
+++ spy_details.py
@@ -1,6 +1,8 @@
-spy_name = 'Bond'
-spy_salutation = 'Mr.'
-spy_name = spy_salutation + " " + spy_name
-spy_age = 21
-spy_rating = 9.9
-spy_is_online = True
+# details of default user.
+spy = {
+    'name': 'Bond',
+    'salutation': 'Mr.',
+    'age': 24,
+    'rating': 4.7,
+    'is_online': True
+}
